edaa2b2a366ad7ccf7a4f403995d7d1f31372e0d
saajid-ahamed <e1398310@u.nus.edu>
2025-08-21 12:45:51 +0800

    Add greeting and farewell

--- src/main/java/Saajid.java
+++ src/main/java/Saajid.java
@@ -1,10 +1,21 @@
 public class Saajid {
+    private static final String NAME = "Saajid";
+    private static final String HORIZONTAL_LINE = "_".repeat(60);
+
+    public void greeting() {
+        System.out.println(Saajid.HORIZONTAL_LINE);
+        System.out.println(String.format("Hello! I'm %s\nWhat can I do for you?", Saajid.NAME));
+        System.out.println("\n" + Saajid.HORIZONTAL_LINE);
+    }
+
+    public void exit() {
+        System.out.println(Saajid.HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println("\n" + Saajid.HORIZONTAL_LINE);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+       Saajid saajid = new Saajid();
+       saajid.greeting();
+       saajid.exit();
     }
 }
