ca31267a1d6aafd63b868bc56ef9115b22145ed3
saajid-ahamed <e1398310@u.nus.edu>
2025-08-21 16:41:53 +0800

    Add support for todo,event,deadline

--- src/main/java/Saajid.java
+++ src/main/java/Saajid.java
@@ -24,6 +24,17 @@ public class Saajid {
         System.out.println(input);
         System.out.println("\n" + Saajid.HORIZONTAL_LINE);
     }
+
+    private void addTask(Task t) {
+        this.tasks[this.taskCount] = t;
+        this.taskCount++;
+        System.out.println(Saajid.HORIZONTAL_LINE);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("   " + t);
+        System.out.println("Now you have " + this.taskCount + " tasks in the list.");
+        System.out.println(Saajid.HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
        Saajid saajid = new Saajid();
        saajid.greeting();
@@ -37,7 +48,7 @@ public class Saajid {
                System.out.println(Saajid.HORIZONTAL_LINE);
                System.out.println("Here are the tasks in your list:");
                for (int i = 1; i <= saajid.taskCount; i++) {
-                   System.out.println(i + ". " + saajid.tasks[i - 1]);
+                   System.out.println(i + "." + saajid.tasks[i - 1]);
                }
                System.out.println("\n" + Saajid.HORIZONTAL_LINE);
            } else if (input.startsWith("mark")) {
@@ -58,13 +69,20 @@ public class Saajid {
                    System.out.println("  " + saajid.tasks[index]);
                    System.out.println("\n" + Saajid.HORIZONTAL_LINE);
                }
-           } else {
-               saajid.tasks[saajid.taskCount] = new Task(input);
-               saajid.taskCount++;
-               System.out.println(Saajid.HORIZONTAL_LINE);
-               System.out.println("added: " + input);
-               System.out.println("\n" + Saajid.HORIZONTAL_LINE);
-           }
+           } else if (input.startsWith("todo ")) {
+            String desc = input.substring(5);
+            Task task = new Todo(desc);
+            saajid.addTask(task);
+        } else if (input.startsWith("deadline ")) {
+            String[] parts = input.substring(9).split(" /by ", 2);
+            Task task = new Deadline(parts[0], parts[1]);
+            saajid.addTask(task);
+        } else if (input.startsWith("event ")) {
+            String[] parts = input.substring(6).split(" /from | /to ");
+            Task task = new Event(parts[0], parts[1], parts[2]);
+            saajid.addTask(task);
+        }
        }
+
     }
 }
